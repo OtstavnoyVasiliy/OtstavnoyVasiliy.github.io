b126f7cfebd312ccb991b6bd40f16d4d34093e7b
OtstavnoyVasiliy <79037020+OtstavnoyVasiliy@users.noreply.github.com>
2022-10-12 19:14:33 +0300

    Create quest5.js

---
+++ quest5.js
@@ -0,0 +1,9 @@
+const price=document.querySelector('.input-price')
+const count=document.querySelector('.input-count')
+const button=document.querySelector('.submit-choose')
+const sum=document.querySelector('.sum')
+
+btn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    sum.innerHTML= (parseInt(price.value) * parseInt(count.value)).toString();
+})
